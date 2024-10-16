55aaadc445ef5f6e759ca26495990108c611c129
Bowater Juliet <juliet.bowater@fibl.org>
2024-10-16 13:38:55 +0200

    first commit

---
+++ FSS_data_treasure_trail.R
@@ -0,0 +1,42 @@
+### Game placeholder
+
+# Hello! Thanks for taking part in the FSS GitHub Game!
+# Git versioning can be thought of as a slightly more technical version of 
+# working with online documents like Google Docs. It allows different users to
+# work on the same project, check that the changes that they make to files do not
+# conflict with the changes that others have made, and see what changes have been 
+# made to files over time. 
+
+# Please do the following:
+# 1) Create a branch (copy) of the FSS game GitHub repo on GitHub
+# 2) Clone your branch to your computer
+# 3) Practice "pulling" your branch from github, to make sure that you are working on 
+#    the latest version of the document (remember that other people can work on it 
+#    too!)
+# 4) Complete the next step of the data treasure trail below. 
+#    Write your name and "Done!" next to it, so that the next person can quickly
+#    see what needs to be done next. Not that in the real world this is not 
+#    required, since Git keeps track of who made which changes and when 
+#    (see the playfully-named command "git blame" for more info).
+# 5) Stage and then commit your changes, with a clear commit message
+# 6) Push your branch back to github
+# 7) Create a merge request and wait for Kevin or Juliet to approve your changes
+#    to the main branch!
+
+# NOTE: If you do not know how to do a step, ping Juliet for the code solution. 
+#       The purpose of the task is for you to learn how to use GitHub, not to
+#       struggle with R data analysis. 
+
+# The data treasure trail
+
+# Task 1) import the iris dataset
+
+# Task 2) how many rows are there in the dataset?
+
+# Task 3) what is the mean of comumn [x]?
+
+# Task 4) create a simple plot of the data
+
+# Task 5) import the farm data
+
+# Task 6) add another row to the farm data; farm_id="f9", area_hectares=5, product="milk" 
